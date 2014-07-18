c01a9feb24f08c3886fc17d73be917e951b5dad6
Aric Camarata <alisalaah@gmail.com>
2014-07-18 17:04:01 +0700

    Added arg validation to three methods of the original package to prevent runtime errors in Meteor

--- suncalc.js
+++ suncalc.js
@@ -26,9 +26,9 @@ var dayMs = 1000 * 60 * 60 * 24,
     J1970 = 2440588,
     J2000 = 2451545;
 
-function toJulian(date) { return date.valueOf() / dayMs - 0.5 + J1970; }
-function fromJulian(j)  { return new Date((j + 0.5 - J1970) * dayMs); }
-function toDays(date)   { return toJulian(date) - J2000; }
+function toJulian(date) { return (date) ? date.valueOf() / dayMs - 0.5 + J1970 : false; }
+function fromJulian(j)  { return (j) ? new Date((j + 0.5 - J1970) * dayMs) : false; }
+function toDays(date)   { return (date) ? toJulian(date) - J2000 : false; }
 
 
 // general calculations for position
